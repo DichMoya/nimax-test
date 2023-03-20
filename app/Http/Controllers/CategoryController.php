a7205ce1fae89e331fd0068a5633a2732d0804a4
DichMoya <aksinlagmail.com>
2023-03-20 23:31:56 +0500

    change Category controller, request and migration

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,42 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Category\StoreCategoryRequest;
-use App\Http\Requests\Category\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\CategoryСollectionResource;
+use App\Http\Services\Category\DeleteCategoryService;
+use App\Http\Services\Category\IndexCategoryService;
+use App\Http\Services\Category\StoreCategoryService;
 use App\Models\Category;
-use App\Models\ProductCategory;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(IndexCategoryService $indexCategoryService): JsonResponse
     {
-        return $this->sendResponse(Category::all(), 'Category retrieved successfully.');
+        return $this->sendResponse(
+            CategoryResource::collection($indexCategoryService->index()),
+            'Category retrieved successfully.');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCategoryRequest $request): JsonResponse
+    public function store(StoreCategoryRequest $request, StoreCategoryService $storeCategoryService): JsonResponse
     {
         $validated = $request->validated();
-        $category = Category::create($validated);
-
-        return $this->sendResponse($category, 'Category created  successfully.');
+        $category = $storeCategoryService->store($validated);
+        return $this->sendResponse(
+            new CategoryResource($category),
+            'Category created  successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category): JsonResponse
+    public function destroy(Category $category, DeleteCategoryService $deleteCategoryService): JsonResponse
     {
-        if($category->products()->count() > 0)
-            return $this->sendError('Cannot be deleted because of the connection to the product.');
-
-        $category->delete();
+        $deleteCategoryService->delete($category);
         return $this->sendResponse([], 'Category deleted successfully.');
     }
 }
